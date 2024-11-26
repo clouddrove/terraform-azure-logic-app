ec41d588f1f7992c7cbd0401144d3a70599eaa57
Vishal Sharma <vishal.sharma@clouddrove.com>
2024-11-26 14:29:33 +0530

    fix: removed lints

--- variables.tf
+++ variables.tf
@@ -6,22 +6,10 @@ variable "name" {
   description = "Name  (e.g. `app` or `cluster`)."
 }
 
-variable "application" {
-  type        = string
-  default     = ""
-  description = "Application (e.g. `cd` or `clouddrove`)."
-}
-
 variable "environment" {
   type        = string
   default     = ""
   description = "Environment (e.g. `prod`, `dev`, `staging`)."
-}
-
-variable "tags" {
-  type        = map(any)
-  default     = {}
-  description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)."
 }
 
 variable "managedby" {
@@ -49,12 +37,6 @@ variable "enabled" {
   description = "Flag to control the module creation."
 }
 
-variable "machine_count" {
-  type        = number
-  default     = 0
-  description = "Number of Virtual Machines to create."
-}
-
 variable "resource_group_name" {
   type        = string
   default     = ""
@@ -67,47 +49,12 @@ variable "location" {
   description = "Location where resource should be created."
 }
 
-variable "create" {
-  type        = string
-  default     = "60m"
-  description = "Used when creating the Resource Group."
-}
-
-variable "update" {
-  type        = string
-  default     = "60m"
-  description = "Used when updating the Resource Group."
-}
-
-variable "read" {
-  type        = string
-  default     = "5m"
-  description = "Used when retrieving the Resource Group."
-}
-
-variable "delete" {
-  type        = string
-  default     = "60m"
-  description = "Used when deleting the Resource Group."
-}
-
 ## Logic App
 
 variable "parameters" {
   description = "The parameters passed to the workflow"
-  default     = {}
-}
-
-variable "source_arm_resource_id" {
-  type        = string
-  default     = ""
-  description = "The ID of the Event Grid System Topic ARM Source. Changing this forces a new Event Grid System Topic to be created. e.g. Storage account id or Resource group id or any serivec id."
-}
-
-variable "storage_account_id" {
-  type        = string
-  default     = ""
-  description = "Storage account id."
+  type        = any
+  default     = null
 }
 
 variable "workflow_schema" {
@@ -118,11 +65,6 @@ variable "workflow_schema" {
 
 variable "workflow_parameters" {
   default     = null
+  type        = any
   description = "Specifies a map of Key-Value pairs of the Parameter Definitions to use for this Logic App Workflow. The key is the parameter name, and the value is a JSON encoded string of the parameter definition (see: https://docs.microsoft.com/azure/logic-apps/logic-apps-workflow-definition-language#parameters)."
-}
-
-variable "parameter" {
-  type        = map(any)
-  default     = null
-  description = "A map of Key-Value pairs."
 }
